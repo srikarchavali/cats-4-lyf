00e55f7af41a24499f33b3002671bda7520e87fd
srikar chavali <chavalisrikar@gmail.com>
2022-01-28 11:11:45 +0000

    added new navbar icons

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,11 +1,18 @@
-import styledComponents from "styled-components"
+// import styledComponents from "styled-components"
+import { FaShoppingCart, FaSignInAlt, FaUserCheck } from "react-icons/fa"
 
 const Navbar = () =>{
     return(
-        <nav>
-            <h2>Menu <i class="fa faShoppingCart" aria-hidden="true"></i></h2>
-            <h2>Cart</h2>
-            <h2>Checkout</h2>
+        <nav className="navbar">
+            <div className="signin">
+                <h3><FaSignInAlt/> Login</h3> 
+            </div>           
+            <div className="cart">
+            <h3><FaShoppingCart/> Cart</h3>
+            </div>
+            <div className="chekout">
+            <h3><FaUserCheck/> Checkout</h3>
+            </div>
         </nav>
     )
 }
