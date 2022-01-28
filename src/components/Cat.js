fbba1a8c4d6405151f6a0b8c80277699a06ed314
srikar chavali <chavalisrikar@gmail.com>
2022-01-28 13:01:05 +0000

    added navbar styling

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import faker from 'faker'
+// import faker from 'faker'
 
 export const Cat = (props) => {
   
@@ -14,7 +14,7 @@ export const Cat = (props) => {
         <h6>{props.temperament}</h6>
       </div>
       <h3>£{props.price}</h3>
-      <button>Add to Cart</button>
+      <button className = 'cartButton'>Add to Cart</button>
     </div>
   );
 };
