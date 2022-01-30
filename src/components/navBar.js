976af16a3920a4e0045da5e4b3464ccbe9aa0271
Akvile4 <aqua0818@gmail.com>
2022-01-30 17:52:29 +0200

    added basket changes

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,20 +1,38 @@
 // import styledComponents from "styled-components"
-import { FaShoppingCart, FaSignInAlt, FaUserCheck } from "react-icons/fa"
+// import styledComponents from "styled-components"
+import { FaSignInAlt, FaUserCheck, FaCat } from 'react-icons/fa';
+import Modal from './Modal';
 
-const Navbar = () =>{
-    return(
-        <nav className="navbar">
-            <div className="signin">
-                <h3><FaSignInAlt/> Login</h3> 
-            </div>           
-            <div className="cart">
-            <h3><FaShoppingCart/> Cart</h3>
-            </div>
-            <div className="chekout">
-            <h3><FaUserCheck/> Checkout</h3>
-            </div>
-        </nav>
-    )
-}
+const Navbar = ({ basketItems, removeItemFromBasket }) => {
+	return (
+		<div className="wrapper">
+			<h3 className="title">
+				<FaCat />
+				Cats4Lyf{' '}
+			</h3>
+			<nav className="navbar">
+				<div className="signin">
+					<h3>
+						<FaSignInAlt /> Login
+					</h3>
+				</div>
+				<div className="cart">
+					<h3>
+						<Modal
+							basketItems={basketItems}
+							removeItemFromBasket={removeItemFromBasket}
+						/>{' '}
+						Cart
+					</h3>
+				</div>
+				<div className="chekout">
+					<h3>
+						<FaUserCheck /> Checkout
+					</h3>
+				</div>
+			</nav>
+		</div>
+	);
+};
 
-export default Navbar
+export default Navbar;
